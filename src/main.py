b2aec2dde371190faf5d5288c760da2dd011cc90
JACaddock <jamesacaddock@gmail.com>
2022-02-06 21:05:41 +0000

    Json handling started
    Made separate functions for handling files and the jsons; readInput, sortJson and outputFile. sortJson being the main function that will sort out the data in a desired format.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,76 @@
-import json
+import orjson, os.path
 
-file = open("data.json")
+def readInput(inplink):
+    inp = open("input/"+inplink+".json", 'rb')
+    data = orjson.loads(inp.read())
+    inp.close()
 
-data = json.load(file)
+    return data
 
-print (data)
+
+def sortJson(inplink, outlink = ""):
+    data = readInput(inplink)
+    newdata = {}
+
+    for i in data:
+        if (i == "place" or i == "entities" or i == "text" or i == "user" or i == 'id' or i == "created_at"):
+            newdata[i] = data[i]
+
+
+    if (outlink != ""):
+        print("outputting")
+        outputFile(outlink, newdata)
+
+    else:
+        print("no output")
+
+    
+def outputFile(outlink, data):
+    out = open("output/"+outlink+".json", 'wb')
+    out.write(orjson.dumps(data))
+    out.close()
+
+
+
+
+if __name__ == "__main__":
+    looping = True
+    while looping:
+        inputlink = input("Enter input file name: ")
+
+        if (inputlink == ""):
+            print("You must use an input file")
+        
+        elif not (os.path.exists("input/"+inputlink+".json")):
+            print("This file does not exist")
+            
+        else:
+            looping = False
+
+
+    looping = True
+    while looping:
+        answer = input("Would you like to output the results?: ")
+        if (answer.lower() == "yes" or answer.lower() == "y"):
+            insideLoop = True
+
+            while insideLoop:
+                outputlink = input("Enter output file name: ")
+
+                if (outputlink == ""):
+                    print("You haven't entered an output name")
+                    answer = input("Are you sure you want to output to a file?: ")
+                    if (answer.lower() == "no" or answer.lower() == "n"):
+                        insideLoop = False
+                        looping = False
+                
+                else:
+                    insideLoop = False
+                    looping = False
+        
+        elif (answer.lower() == "no" or answer.lower() == "n"):
+            looping = False
+            outputlink = ""
+
+
+    sortJson(inputlink, outputlink)
